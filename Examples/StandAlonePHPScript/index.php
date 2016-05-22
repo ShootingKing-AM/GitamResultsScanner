fabcef73ea6906c89f3e0f951563e7b2a99dc673
ShootingKing-AM <funnyboy313@gmail.com>
2016-05-22 14:21:09 +0530

    Update v2.0
    Update to extract results from new Git Report Gird Viewer.

--- Examples/StandAlonePHPScript/index.php
+++ Examples/StandAlonePHPScript/index.php
@@ -1,54 +1,117 @@
 <?php
-	header('Content-Type: text/plain');
+	// header('Content-Type: text/plain');
 	ignore_user_abort(true);
 	set_time_limit(0);
 
 	unset( $_POST );
 	include_once './ResultsScanner.php'; // Assuming ResultsScanner is in the same directory as this file
 	
-	$years = array( 8, 09, 10, 11, 12, 13, 14, 15 );
+	$branches = array( 1, 2, 3, 4, 5, 6, 7, 8 );
+ 	$years = array( 8, 09, 10, 11, 12, 13, 14, 15 );
 	$sections = array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 );
 	$sems = array( 1, 2, 3, 4, 5, 6, 7, 8 );
+		
+	// $backupFilename = getcwd().'/last';
+	// $iRBack = 0;
+	// if( file_exists($backupFilename) )
+	// {
+		// $handle = fopen( $backupFilename, 'r' );
+		// $iRBack = fgets( $handle, 10 );
+		// fclose($handle);
+	// }
+	// $iR = '';
+	$db = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_DBNAME );
 	
-	// var $iR; //= '12101'+((years[i]<10)?('0'+years[i]):(years[i]))+sections[j]+((iStud<10)?('0'+iStud):(iStud));
-	// int $res = 0;
-	
-	$filename = 'log.txt';	
-	
-	for( $i = 0; $i < sizeof($years); $i++ )
+	function LogToFile($logstr)
 	{
-		for( $j = 0; $j < sizeof($sections); $j++ )
+		$filename = 'log.txt';
+		$handle = fopen($filename, 'a');
+		fwrite($handle, $logstr);
+		fclose($handle);
+	}
+	
+	if( is_bool($db) )
+	{
+		LogToFile("Could not connect to Database... \n");
+	}
+	
+	$ThisClassExists = false;
+	
+	for( $k = 0; $k < sizeof($branches); $k++ )
+	{	
+		for( $i = 0; $i < sizeof($years); $i++ )
 		{
-			for( $l = 0; $l < sizeof($sems); $l++ )
+			$ThisClassExists = false;
+			for( $j = 0; $j < sizeof($sections); $j++ )
 			{
-				for( $iStud = 1; $iStud <= 66; $iStud++ )
-				{					
-					$iR = '12101'.(($years[$i]<10)?('0'.$years[$i]):($years[$i])).$sections[$j].(($iStud<10)?('0'.$iStud):($iStud));
-					$res = getResultFromServer($iR, $sems[$l], 2);
-					//echo 'Queried...'.$iR;
-					$logstr = '';
-					if( $res == 1 )
+				if( $j != 0 && !$ThisClassExists)
+				{
+					LogToFile('Skipping sections from 1210'.$branches[$k].(($years[$i]<10)?('0'.$years[$i]):($years[$i])).$sections[$j].('01'));
+					continue;
+				}
+				
+				for( $l = 0; $l < sizeof($sems); $l++ )
+				{
+					if( $l != 0 && !$ThisClassExists)
 					{
-						$logstr = "Queried...$iR [".$sems[$l]."]...Successful!\r\n";
+						LogToFile("Skipping Sem-". $sems[$l]. " for section-" . $sections[$j] . ' from 1210'.$branches[$k].(($years[$i]<10)?('0'.$years[$i]):($years[$i])).$sections[$j].('01'));
+						continue;
 					}
-					else if( $res == 2 )
-					{						
-						$logstr = "Queried...$iR [".$sems[$l]."]...ERROR: Network Error!\r\n";
-					}
-					else if( $res == 3 )
+					
+					for( $iStud = 1; $iStud <= 66; $iStud++ )
 					{
-						$logstr = "Queried...$iR [".$sems[$l]."]...Already Exiting in DB... Skipping!\r\n";
+						$iR = '1210'.$branches[$k].(($years[$i]<10)?('0'.$years[$i]):($years[$i])).$sections[$j].(($iStud<10)?('0'.$iStud):($iStud));
+						/* echo intval($iR).'<'.intval($iRBack).'<br/>';
+						if( intval($iR) < intval($iRBack) )
+							continue; */
+						
+						$res = getResultFromServer($db, $iR, $sems[$l], 2);
+						//echo 'Queried...'.$iR;
+						$logstr = '';
+						if( $res == 1 )
+						{
+							$logstr = "Queried...$iR [".$sems[$l]."]...Successful!\r\n";
+							if( $ThisClassExists == false )
+							{
+								$ThisClassExists = true;
+							}
+						}
+						else if( $res == 2 )
+						{						
+							$logstr = "Queried...$iR [".$sems[$l]."]...ERROR: Network Error!\r\n";
+						}
+						else if( $res == 3 )
+						{
+							$logstr = "Queried...$iR [".$sems[$l]."]...Already Exiting in DB... Skipping!\r\n";
+							if( $ThisClassExists == false )
+							{
+								$ThisClassExists = true;
+							}
+						}
+						else if( $res == 0 )
+						{
+							$logstr = "Queried...$iR [".$sems[$l]."]...ERROR: Invalid Details!\r\n";
+						}
+						// echo $logstr;
+						LogToFile($logstr);
 					}
-					else if( $res == 0 )
-					{
-						$logstr = "Queried...$iR [".$sems[$l]."]...ERROR: Invalid Details!\r\n";
-					}
-					// echo $logstr;
-					$handle = fopen($filename, 'a');
-					fwrite($handle, $logstr);
-					fclose($handle);
-					// exit;
+					
 				}
 			}
 		}
-	}				
+	}
+	mysqli_close($db);
+	
+	
+	/*function shutdown()
+	{
+		if( $iR != '' )
+		{
+			$handle = fopen($backupFilename, 'w');
+			fwrite($handle, $iR);
+			fclose($handle);
+		}
+		echo 'Shutting down ....';
+	}
+
+	register_shutdown_function('shutdown');	*/
